0631c9e2977f69e835e3dcbd1bd3fc6780faa2f4
Verscheure Quentin <verscheure.quentin@gmail.com>
2022-06-15 22:15:17 +0200

    git post test rest DB

--- src/DataFixtures/GiteFixtures.php
+++ src/DataFixtures/GiteFixtures.php
@@ -11,19 +11,31 @@ class GiteFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i=0; $i < 30; $i++) { 
+
+
+        for ($i=0; $i < 20; $i++) { 
+
+            $randomPictureNumber = random_int(1,5);
+            $setPicture="";
+            for ($j=0; $j < $randomPictureNumber; $j++) {
+                $setPicture .= "picture(" . random_int(1,68) . ")";
+                $setPicture .= ",";
+            }
+
+            $description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a est sem. Cras finibus velit ac sem varius ultricies. Curabitur aliquet lacus metus, et dictum orci faucibus ut. Curabitur suscipit vel leo sit amet efficitur. Morbi nec cursus risus, a finibus dui. Aliquam mollis nisl a porttitor pellentesque. Etiam tincidunt vestibulum tempus.";
+
             $gite = new Gite();
             $gite
                 ->setName("gite" . $i)
-                ->setDescription("description gite n°" . $i)
+                ->setDescription("description gite n°". $i . $description )
                 ->setContact("contact gite n°" . $i)
                 ->setScheduleContact("horaire gite n°" . $i)
                 ->setLocalisation("localisation gite n°" . $i)
                 ->setSurface(random_int(30,300))
                 ->setNumberOfBedroom(random_int(1,5))
                 ->setNumberOfBed(random_int(1,7))
-                ->setPets(true);
-
+                ->setPets((bool)rand(0,1))
+                ->setPicture($setPicture);
                 $manager->persist($gite);
         }
 
